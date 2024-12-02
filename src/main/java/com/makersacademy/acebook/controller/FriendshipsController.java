5a6f386acb8cf93da7181609946cafaf5d3db0a9
“alexagodzilla” <alexagozzino@gmail.com>
2024-12-02 13:13:58 +0000

    Friendship Request sender by Alessandro & Shaeera

---
+++ src/main/java/com/makersacademy/acebook/controller/FriendshipsController.java
@@ -0,0 +1,49 @@
+package com.makersacademy.acebook.controller;
+
+import com.makersacademy.acebook.model.Friendship;
+import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.model.UserService;
+import com.makersacademy.acebook.repository.FriendshipRepository;
+import com.makersacademy.acebook.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Controller
+public class FriendshipsController {
+
+    @Autowired
+    private FriendshipRepository friendshipRepository;
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+
+
+    @PostMapping("/search")
+    public RedirectView requestFriendship(@RequestParam Long receiverId){
+        String username = userService.getAuthenticatedUserEmail();
+        User userDetails = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Long userId = userDetails.getId();
+
+        Friendship friendshipRequest = new Friendship();
+        friendshipRequest.setSenderId(userId);
+        friendshipRequest.setStatus("pending");
+        friendshipRequest.setReceiverId(receiverId);
+        friendshipRequest.setCreatedAt(LocalDateTime.now());
+        friendshipRepository.save(friendshipRequest);
+        return new RedirectView("/search");
+    }
+
+}
