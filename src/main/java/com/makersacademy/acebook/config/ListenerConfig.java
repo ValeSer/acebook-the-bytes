452207548e263597a5310b447ee92c43d7ab76e8
Valeria <valeria.serci.uk@gmail.com>
2024-12-04 12:02:13 +0000

    notification for comments in working order

--- src/main/java/com/makersacademy/acebook/config/ListenerConfig.java
+++ src/main/java/com/makersacademy/acebook/config/ListenerConfig.java
@@ -1,7 +1,9 @@
 package com.makersacademy.acebook.config;
 
+import com.makersacademy.acebook.listener.CommentListener;
 import com.makersacademy.acebook.listener.FriendshipListener;
 import com.makersacademy.acebook.repository.NotificationRepository;
+import com.makersacademy.acebook.repository.PostRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,9 +18,15 @@ public class ListenerConfig {
     @Autowired
     private NotificationRepository notificationRepository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @PostConstruct
     public void configureListeners() {
         FriendshipListener.setUserRepository(userRepository);
         FriendshipListener.setNotificationRepository(notificationRepository);
+        CommentListener.setPostRepository(postRepository);
+        CommentListener.setUserRepository(userRepository);
+        CommentListener.setNotificationRepository(notificationRepository);
     }
 }
