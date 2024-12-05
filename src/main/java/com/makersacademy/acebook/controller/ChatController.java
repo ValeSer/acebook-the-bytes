3fafde0188e9f683a7a19004a2bf552b6a12a34b
“alexagodzilla” <alexagozzino@gmail.com>
2024-12-05 15:31:51 +0000

    chat show page working

--- src/main/java/com/makersacademy/acebook/controller/ChatController.java
+++ src/main/java/com/makersacademy/acebook/controller/ChatController.java
@@ -1,15 +1,20 @@
 package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.Chat;
+import com.makersacademy.acebook.model.Message;
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.ChatRepository;
+import com.makersacademy.acebook.repository.MessageRepository;
 import com.makersacademy.acebook.repository.UserRepository;
 import com.makersacademy.acebook.service.UserService;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.HashMap;
 import java.util.List;
@@ -27,6 +32,9 @@ public class ChatController {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    MessageRepository messageRepository;
 
     @GetMapping("/chats")
     public String allCurrentUserChats(Model model) {
@@ -49,5 +57,14 @@ public class ChatController {
         model.addAttribute("allChatsIdsAndOtherPersonIds", otherPersonsIdsAndChatId);
         return "chats/index";
     }
+
+
+    @GetMapping("/chat/{id}")
+    public String showAllMessages(Model model, @PathVariable Long id) {
+        Iterable<Message> messages = messageRepository.findByChatIdOrderByCreatedAtAsc(id);
+        model.addAttribute("allMessagesFromBothUsers", messages);
+        return "chats/show";
+    }
+
 
 }
